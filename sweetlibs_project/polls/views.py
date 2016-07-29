6fcb50a739b356509ad6592d793b2a656a4957a1
Matthew Scott <matthewruelscott@gmail.com>
2016-07-29 16:15:53 -0400

    removed fizzbuzz

--- sweetlibs_project/polls/views.py
+++ sweetlibs_project/polls/views.py
@@ -2,16 +2,3 @@ from django.shortcuts import render
 
 def index(request):
      return render(request, 'polls/index.html', {})
-
-
-# def index (request):
-#     if first % 3 == 0:
-#         word += 'Fizz'
-#     if first % 5 == 0:
-#         word += 'Buzz'
-#     return HttpResponse()
-#
-#     if first < last:
-#         index(first + 1, last)
-#
-# index(0, 100)
